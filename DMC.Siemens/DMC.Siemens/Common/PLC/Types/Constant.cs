95cc2711d89bb2be1ddfa4b8f30ec18646142644
jeffreym <jeffreym@7230e100-e0b5-d749-8082-3a14de58cf53>
2016-11-23 11:05:49 +0000

    * Changed Constant<T> to a struct very similar to Nullable<T> so that it is easier to use
    git-svn-id: https://svn.dmcinfo.com/svn/DMC.Siemens.NET.SVN/trunk@7 7230e100-e0b5-d749-8082-3a14de58cf53

--- DMC.Siemens/DMC.Siemens/Common/PLC/Types/Constant.cs
+++ DMC.Siemens/DMC.Siemens/Common/PLC/Types/Constant.cs
@@ -6,39 +6,97 @@ using System.Threading.Tasks;
 
 namespace DMC.Siemens.Common.PLC.Types
 {
-    public class Constant<T> where T : struct
+    public struct Constant<T> where T : struct
     {
 
         #region Constructors
 
-        public Constant()
-        {
-        }
-
         public Constant(T value)
         {
-            this.Value = value;
+            this._Value = value;
+            this._HasValue = true;
+            this._Name = null;
         }
 
         public Constant(string name)
         {
-            this.Name = name;
+            this._Value = default(T);
+            this._Name = name;
+            this._HasValue = false;
         }
 
         #endregion
 
         #region Public Properties
 
-        public T? Value { get; set; }
+        private T _Value;
+        public T Value
+        {
+            get
+            {
+                if (!this.HasValue)
+                {
+                    throw new InvalidOperationException("Cannot retrieve a constant with no value.");
+                }
+                return _Value;
+            }
+        }
 
-        public string Name { get; set; }
+        private string _Name;
+        public string Name
+        {
+            get
+            {
+                return this._Name;
+            }
+            set
+            {
+                this._Name = value;
+            }
+        }
 
+        private bool _HasValue;
         public bool HasValue
         {
             get
             {
-                return this.Value.HasValue;
+                return this._HasValue;
             }
+            private set
+            {
+                this._HasValue = value;
+            }
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public override bool Equals(object obj)
+        {
+            if (!this.HasValue) return (obj == null);
+            else if (obj == null) return false;
+            return this.Value.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HasValue ? this.Value.GetHashCode() : 0;
+        }
+
+        public override string ToString()
+        {
+            return HasValue ? this.Value.ToString() : "";
+        }
+
+        public static implicit operator Constant<T>(T value)
+        {
+            return new Constant<T>(value);
+        }
+
+        public static explicit operator T(Constant<T> value)
+        {
+            return value.Value;
         }
 
         #endregion
