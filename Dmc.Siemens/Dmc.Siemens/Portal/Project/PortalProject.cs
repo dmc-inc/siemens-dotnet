c06825b695238ba86207216eb59be17dcf5a50f2
jmccormi7 <jmccormi7@gmail.com>
2017-04-24 22:07:25 -0500

    * Fixed dumb implementation of PortalProject

--- Dmc.Siemens/Dmc.Siemens/Portal/Project/PortalProject.cs
+++ Dmc.Siemens/Dmc.Siemens/Portal/Project/PortalProject.cs
@@ -4,16 +4,14 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dmc.Siemens.Common.Base;
 using Dmc.Siemens.Common.Interfaces;
 using Dmc.Wpf.Base;
 
 namespace Dmc.Siemens.Portal.Project
 {
-	public class PortalProject : NotifyPropertyChanged, IProject
+	public class PortalProject : ProjectBase, IProject
 	{
-		public IEnumerable<IAutomationObject> AutomationObjects => throw new NotImplementedException();
-
-		public string Name { get; set; }
-
+		
 	}
 }
